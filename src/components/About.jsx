1fc6de94bbbeff8f2f7e787749129fcdeba42a3b
Kristián Slovák <slovak.kristian1@gmail.com>
2023-04-21 15:09:14 +0200

    Footer finished, about has text but needs CSS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,40 +2,92 @@ import React from "react";
 
 function About({ aboutRef }) {
   return (
-    <div
-      ref={aboutRef}
-      id="About"
-      className="w-full h-screen flex items-center justify-center bg-white z-30"
-    >
-      <div className="flex flex-col items-start justify-center w-5/12 px-20 bg-gradient-to-br from-cyan-500 h-4/6 my-20 to-sky-600 gap-24">
-        <img
-          src={require("../assets/KristianSlovak.png")}
-          alt="Kristián Slovák"
-          className="h-32 w-32 rounded-full border-black"
-        />
-        <div className="flex flex-col gap-3">
-          <h4>ABOUT</h4>
-          <p>
-            My name is Kristián. I graduated from Green Fox Academy and now I
-            work on few projects while looking for job as a Junior developer.
-          </p>
+    <div className="w-screen h-screen flex" ref={aboutRef}>
+      <div
+        id="About"
+        className="w-1/2 h-screen flex items-center justify-end bg-gradient-to-br from-blue-500 to-cyan-500  gap-24"
+      >
+        <div className="flex flex-col items-start justify-center w-5/6 px-20 gap-24 bg-white h-5/6 relative">
+          <img
+            src={require("../assets/react.png")}
+            alt="react"
+            className="absolute left-2 top-2 w-16 h-16"
+          />
+          <img
+            src={require("../assets/javascript.png")}
+            alt="Javascript"
+            className="absolute bottom-2 left-2 w-16 h-16"
+          />
+          <img
+            src={require("../assets/KristianSlovak.png")}
+            alt="Kristián Slovák"
+            className="h-36 w-36 rounded-full border-black"
+          />
+          <div className="flex flex-col gap-3">
+            <h4>ABOUT</h4>
+            <p>
+              My name is Kristián. I graduated from Green Fox Academy and now I
+              work on few projects while looking for job as a Junior developer.
+            </p>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h4>TECHNOLOGIES</h4>
+            <p>
+              JavaScript, TypeScript, TailWind, CSS, React, Node.js, Express,
+              MongoDB, MySQL, Git, GitHub, Jest...
+            </p>
+          </div>
         </div>
       </div>
-      <div className="w-5/12 flex flex-col gap-24 items-start px-20 justify-center bg-sky-100 h-4/6 z-30">
-        <div className="flex flex-col gap-3">
-          <h4>TECHNOLOGIES</h4>
-          <p>
-            JavaScript, TypeScript, TailWind, CSS, React, Node.js, express,
-            MongoDB, MySQL, Git, HitHub, Jest ...
-          </p>
-        </div>
-        <div className="flex flex-col gap-3">
-          <h4>O NÁS</h4>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Repellendus fugiat expedita eveniet quia assumenda eaque laudantium
-            itaque, est qui voluptate!
-          </p>
+      <div className="w-1/2 h-screen flex items-center justify-start bg-blue-100 gap-24">
+        <div className="w-5/6 flex flex-col gap-24 items-start px-20 justify-center bg-white h-5/6 relative">
+          <img
+            src={require("../assets/CSS3.png")}
+            alt="CSS"
+            className="absolute bottom-2 right-2 w-16 h-20"
+          />
+          <img
+            src={require("../assets/HTML.png")}
+            alt="HTML"
+            className="absolute top-2 right-2 w-16 h-16"
+          />
+          <div className="flex flex-col gap-3">
+            <h4>MOTIVATION</h4>
+            <p>
+              I chose web development as my career because I've worked in
+              entry-level jobs with limited room for growth. I learned there to
+              work under pressure and to be effective. I enjoy finding simple
+              solutions for complex problems and I am passionate about tech and
+              enjoy learning new things.
+            </p>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div>
+              <h4>Projects</h4>
+            </div>
+            <div>
+              <div>
+                <a
+                  href="https://weather-app-nu-steel.vercel.app/"
+                  className="hover:text-sky-600 transition duration-100 text-black"
+                >
+                  Weather App
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://KristianSlovak.github.io/Personalized-CV"
+                  className="hover:text-sky-600 transition duration-100 text-black"
+                >
+                  Personal CV Page (this page)
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h4>HOBBIES</h4>
+            <p>Gym, Coding, Computer games, Movies/Shows...</p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,7 +34,7 @@ function Footer({ footerRef }) {
     <div
       ref={footerRef}
       id="Links"
-      className="w-full flex items-center gap-3 bg-zinc-300 h-60 z-20"
+      className="w-full flex items-center gap-3 bg-zinc-300 h-52 z-20"
     >
       <div className="flex pl-10 gap-3 flex-col w-1/2">
         <h4 className="text-2xl">Kristián Slovák</h4>
@@ -42,34 +42,54 @@ function Footer({ footerRef }) {
           My name is Kristián Slovák and this is my personalized CV webpage.
         </p>
       </div>
-      <div className="w-1/2 flex pl-10 gap-3">
-        <ul className="w-1/2 flex pl-10 gap-3 flex-col">
-          <h4 className="text-2xl">Links & Contact</h4>
-          <li className="hover:text-sky-600 hover:scale-125 transition duration-100 w-fit">
-            <a href="https://www.linkedin.com/in/kristi%C3%A1n-slov%C3%A1k-58870124a/">
-              LinkedIn
-            </a>
-          </li>
-          <li className="hover:text-sky-600 hover:scale-125 transition duration-100 w-fit">
-            <a href="https://github.com/KristianSlovak">GitHub</a>
-          </li>
-        </ul>
-        <ul className=" w-1/2 flex pl-10 gap-2 flex-col">
-          <li className="hover:text-sky-600 hover:scale-125 transition duration-100 w-fit">
-            <a href="https://www.codewars.com/users/KristianSlovak">Codewars</a>
-          </li>
-          <div>
-            <li className="hover:cursor-pointer hover:text-sky-600 transition duration-100 w-fit">
-              Download CV
+      <div className="w-1/2 flex items-center flex-col pl-10 gap-3">
+        <h4 className="text-2xl">Links & Contact</h4>
+        <div className="flex">
+          <ul className="w-1/2 flex flex-col pl-10 gap-3">
+            <li className="flex hover:text-sky-600 hover:scale-125 transition duration-100 w-fit">
+              <img
+                src={require("../assets/icons8-linkedin-24.png")}
+                alt="LinkedIn"
+              />
+              <a href="https://www.linkedin.com/in/kristi%C3%A1n-slov%C3%A1k-58870124a/">
+                LinkedIn
+              </a>
+            </li>
+            <li className="flex hover:text-sky-600 hover:scale-125 transition duration-100 w-fit">
+              <img
+                src={require("../assets/icons8-github-24.png")}
+                alt="GitHub"
+              />
+              <a href="https://github.com/KristianSlovak">GitHub</a>
+            </li>
+            <li className="flex hover:text-sky-600 hover:scale-125 transition duration-100 w-fit">
+              <img
+                src={require("../assets/icons8-codewars-24.png")}
+                alt="codewars"
+              />
+              <a href="https://www.codewars.com/users/KristianSlovak">
+                Codewars
+              </a>
+            </li>
+          </ul>
+          <ul className=" w-1/2 flex flex-col pl-10 gap-2">
+            <li className="flex hover:cursor-pointer hover:text-sky-600 transition duration-100 w-fit">
+              <img src={require("../assets/icons8-cv-24.png")} alt="CV" />
+              <p>Download CV</p>
             </li>
             <li
               onClick={handleCopy}
               ref={emailRef}
               onMouseEnter={handleHidden}
               onMouseLeave={handleHidden}
-              className="hover:cursor-pointer hover:text-sky-600 transition duration-100 w-fit"
+              className="flex items-center hover:cursor-pointer hover:text-sky-600 transition duration-100 w-fit"
             >
-              Email: slovak.kristian1@gmail.com
+              <img
+                src={require("../assets/icons8-mail-24.png")}
+                alt="Mail"
+                className="w-6 h-6"
+              />
+              <p>Email: slovak.kristian1@gmail.com</p>
             </li>
             <p
               ref={pRef}
@@ -77,12 +97,12 @@ function Footer({ footerRef }) {
                 clicked
                   ? "bg-emerald-400 bg-opacity-20"
                   : "bg-black bg-opacity-20"
-              }   w-fit px-4 text-sm absolute ${hidden ? "visible" : "hidden"}`}
+              }   w-fit px-4 text-sm ${hidden ? "visible" : "hidden"}`}
             >
               Copy to clipboard
             </p>
-          </div>
-        </ul>
+          </ul>
+        </div>
       </div>
     </div>
   );
