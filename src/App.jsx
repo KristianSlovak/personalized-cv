0762e403878a5fab6a1df2d84d38cf5210797273
Kristián Slovák <slovak.kristian1@gmail.com>
2024-02-06 21:22:59 +0100

    Started Projects Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/Navbar/NavBar";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -11,6 +12,17 @@ function App() {
       <h1>I'm Kristián Slovák - curious</h1>
       <h1>designer and developer</h1>
       <h1>based in Piešťany, Slovakia</h1>
+      <div className="links">
+        <a href="something">Github</a>
+        <a href="something">LinkedIn</a>
+        <a href="something">Twitter</a>
+        <a href="something">Blog</a>
+        <a href="something">Email</a>
+      </div>
+      <div className="projects">
+        <h1>Projects</h1>
+        <Projects />
+      </div>
       <p className="random">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
         corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
