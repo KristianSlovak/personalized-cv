9bc6f63c00d672c3cd8734038100e7c14664d42e
Kristián Slovák <slovak.kristian1@gmail.com>
2024-02-06 15:15:08 +0100

    Navbar needs mobile and multiple dis design

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,74 @@ import NavBar from "./components/Navbar/NavBar";
 
 function App() {
   return (
-    <div id="main-div" className="main-div">
+    <div id="main-div" className="container">
+      <div className="border-wrap"></div>
       <NavBar />
+      <h1>Hello 👋</h1>
+      <h1>I'm Kristián Slovák - curious</h1>
+      <h1>designer and developer</h1>
+      <h1>based in Piešťany, Slovakia</h1>
+      <p className="random">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <p className="random">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero pariatur
+        corporis quaerat voluptatum eos tempora temporibus nisi voluptates sed,
+        exercitationem sequi dolore culpa incidunt accusamus, quasi unde
+        reprehenderit ea molestias.
+      </p>
+      <div className="border-wrap-bottom"></div>
     </div>
   );
 }
